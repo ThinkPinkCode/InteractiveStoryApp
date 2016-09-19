9fe3a38ff92cde06b5e9dae336dba2d94b1bbaf8
Melanie Myers <MMelanieJ@gmail.com>
2016-09-19 19:26:11 -0400

    added page and choice classes

--- app/src/main/java/com/teamtreehouse/interactivestory/UI/StoryActivity.java
+++ app/src/main/java/com/teamtreehouse/interactivestory/UI/StoryActivity.java
@@ -1,13 +1,11 @@
-package com.teamtreehouse.interactivestory;
+package com.teamtreehouse.interactivestory.UI;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
+
+import com.teamtreehouse.interactivestory.R;
 
 public class StoryActivity extends AppCompatActivity {
 
